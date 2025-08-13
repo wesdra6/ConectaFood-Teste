47512efef063f9018c905ced677882c607d6c9d7
wesdra6 <119011690+wesdra6@users.noreply.github.com>
2025-08-13 17:20:15 -0300

    Update dev-secrets.js

--- app/js/dev-secrets.js
+++ app/js/dev-secrets.js
@@ -1,6 +1,6 @@
 // ARQUIVO SECRETO: app/js/dev-secrets.js
 
 const DEV_SUPABASE_URL = 'https://ferramentas-supabase-demos.lblzl4.easypanel.host/';
-const DEV_SUPABASE_ANON_KEY = 'SUA_CHAVE_ANON_AQUI';
+const DEV_SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICJyb2xlIjogImFub24iLAogICJpc3MiOiAic3VwYWJhc2UiLAogICJpYXQiOiAxNzM3NDI4NDAwLAogICJleHAiOiAxODk1MTk0ODAwCn0.xSC1u_LpBqMbarBrlsMS_adc9JVzyiHETWOnzopJMDs';
 
 const DEV_API_BASE_URL = 'https://n8n-webhook.uptecnology.com.br/webhook/';
